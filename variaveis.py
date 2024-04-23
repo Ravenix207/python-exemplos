74c7d94911cf03a8079f3656b3c7f8ea6d58f535
Ravenix207 <mariaclarar2908@gmail.com>
2024-04-23 16:41:42 -0300

    criação variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,12 @@
+nome="Maria Clara"
+idade=16
+email='mariaclarar608@gmail.com'
+telefone='17 99999-0000'
+nota=8.99
+aprovado=True
+print(nome)
+print(idade)
+print(email)
+print(telefone)
+print(nota)
+print(aprovado)
