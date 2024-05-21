26cf2f6b3ab660efce2c1f197363dc3566328048
Ravenix207 <mariaclarar2908@gmail.com>
2024-05-21 14:55:32 -0300

    criacao_lista.py

---
+++ lista.py
@@ -0,0 +1,8 @@
+aluno = ['Maria Clara Rodrigues ', 6, 1.0, False]
+print(f"Nome: {aluno[0]}")
+print(f'Faltas: {aluno[1]}')
+print(f'Média: {aluno[2]}')
+if aluno[3] == True:
+    print(f'Aprovado: Sim')
+else:
+    print(f'Aprovado: Não')
