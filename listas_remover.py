f0e5a8458028401093f76e2a419d0f47e0a041f6
Ravenix207 <mariaclarar2908@gmail.com>
2024-05-23 09:37:34 -0300

    Criacao_remover.py

---
+++ listas_remover.py
@@ -0,0 +1,15 @@
+alunos = ['Adriana', 'João', 'Hiago', 'Thiago', 'Gael', 'Juliana', 'Adriana']
+print(f' Lista de alunos: {alunos}')
+# Método remover()
+alunos.remove('Adriana')
+print(f' Lista de alunos: {alunos}')
+#Método pop
+alunos.pop(2)
+print(f' Lista de alunos: {alunos}')
+# Função del
+del alunos[3]
+print(f' Lista de alunos:{alunos}')
+# Método clear
+alunos.clear()
+print(f' Lista de alunos: {alunos}')
+
